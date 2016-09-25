1a9420fbb1cacdb37c7cd6a754360d4d74036140
Philippe Vienne <philippegeek@gmail.com>
2016-09-25 11:14:29 +0200

    Add tickets

---
+++ app/models/ticket.rb
@@ -0,0 +1,27 @@
+class Ticket < ApplicationRecord
+  belongs_to :pass
+  has_one :event, through: :pass
+  has_one :organization, through: :pass
+
+  validates_presence_of :status
+  validates_inclusion_of :status, in: %w(buying bought used refunded canceled)
+
+  before_validation do
+    if status.nil?
+      write_attribute :status, 'buying'
+    end
+  end
+
+  scope :valid_for_access, -> {where(status: %w(bought)).includes(:pass).where('passes.entry_closed > ?', Time.now)}
+  scope :with_status, -> (status) {where(status: status)}
+
+  def self.clean_buying_tickets
+    Ticket.with_status('buying').where('updated_at < ?', 12.hours.ago).delete_all
+  end
+
+  def self.statuses
+    %w(buying bought used refunded canceled).map do |status|
+      [I18n.t("ticket.status.#{status}", default: I18n.t("status.#{status}", default: status.to_s.humanize))]
+    end
+  end
+end
---
+++ db/migrate/20160925090059_create_tickets.rb
@@ -0,0 +1,12 @@
+class CreateTickets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tickets do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
