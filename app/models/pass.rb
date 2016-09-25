575df006005f81025b9e55c6deda530caa481d02
Philippe Vienne <philippegeek@gmail.com>
2016-09-25 13:48:49 +0200

    Add checks for tickets options

--- app/models/pass.rb
+++ app/models/pass.rb
@@ -2,7 +2,7 @@ class Pass < ApplicationRecord
   belongs_to :event
   has_one :organization, through: :event
   has_many :tickets
-  has_many :ticket_options
+  has_and_belongs_to_many :ticket_options
 
   include StatusConcern
   use_statuses %w(public invitation closed public_va invitation_va), default: :closed
--- app/models/ticket_option.rb
+++ app/models/ticket_option.rb
@@ -2,4 +2,13 @@ class TicketOption < ApplicationRecord
   has_and_belongs_to_many :tickets
   has_and_belongs_to_many :passes
   belongs_to :event
+
+  include StatusConcern
+  use_statuses %w(opened closed), default: :opened
+
+  validates_presence_of :event
+
+  def real_tickets
+    tickets.where(status: [:bought, :used])
+  end
 end
